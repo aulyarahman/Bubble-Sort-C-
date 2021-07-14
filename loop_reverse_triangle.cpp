98352befab28eefee7596d85b92b731fdb9f9d31
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2021-07-14 20:04:56 +0800

    'sdsd

--- loop_reverse_triangle.cpp
+++ loop_reverse_triangle.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-  int rows, spaces
+int main(){
+  int rows, space;
 
   cout << "Masukkan jumlah baris : ";
   cin >> rows;
 
-  for(int i = 1; k <= n; i++){
-    // 
+  for(int i = 1 ; i >= rows; i++) {
+    
   }
 }
